4c281b257fe7569d41d0cf0c007dea738c5b1181
hasanali8334 <hasanali83@gmail.com>
2021-06-21 00:46:29 +0300

    s

---
+++ client/pages/webcamrtc.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="photobooth">
+    <div class="controls">
+      <button @click="startrecord()">Record</button>
+      <button @click="stoprecord()">Stop Record</button>
+    </div>
+
+    <canvas class="photo"></canvas>
+    <video class="player"></video>
+    <div class="strip"></div>
+  </div>
+</template>
+
+<script>
+  import RecordRTC from 'recordrtc'
+  
+  
+
+const chunks=[];
+export default {
+  head() {},
+  data() {},
+  
+  methods: {
+      startrecord(){
+         let video = document.querySelector(".player");
+      navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(async function(stream) {
+    let recorder = RecordRTC(stream, {
+        type: 'video'
+    });
+    recorder.startRecording();   
+            
+    });
+      },
+      stoprecord(){
+        recorder.stopRecording();
+          let blob = recorder.getBlob();
+          console.log(blob);
+      },
+
+    getVideo() {
+        
+      
+
+    
+  },
+  beforeMount() {
+    
+    this.getVideo();
+  }
+  
+}
+</script>
